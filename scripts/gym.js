946307e32779259e71c95f2ea02213260fa02796
Anderson Ribeiro Nunes <andersonribeironunes@hotmail.com>
2021-06-04 08:49:16 -0300

    css

--- scripts/gym.js
+++ scripts/gym.js
@@ -1,7 +1,7 @@
 var formulario = document.getElementById('formulario')
 
 function salvarNome(evento) {
-  evento.preventDefault()
+  evento.preventDefault() //preventDefault cancela o evento se for cancelável, sem parar a propagação do mesmo
   var nome = document.getElementById('name').value
   //localStorage salva chave e valor no navegador
   localStorage.setItem('nome', nome)
