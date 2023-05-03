3151f1592079d20f147acd3ce276b60bb7a3a4f0
anderson-nunes <andersonribeironunes@hotmail.com>
2023-05-03 10:42:19 -0300

    Finished academy project

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,45 +1,45 @@
 var formulario = document.getElementById('formulario')
 
 var todosTreinos = [{
-    nome: 'Peito',
-    id: 'peito',
-    exercicios: [`Supino inclinado com halteres`,
-      `Supino reto com barra`,
-      'Afundos nas barras paralelas',
-      'Crossover com pega alta',
-      'Flexões'
-    ]
-  },
-  {
-    nome: 'Costas',
-    id: 'costas',
-    exercicios: ['Puxada aberta no pulley', 'Flexão de ombor no cross', 'Remada com corda na polia alta', 'Remada unilateral na polia alta', 'Remada unilateral apoiada no banco'],
-  },
-  {
-    nome: 'Tríceps',
-    id: 'tricips',
-    exercicios: ['Tríceps na testa',
-      'Tríceps corda na polia',
-      'Apoio no solo com as mãos fechadas(flexão militar)',
-      'Supino fechado',
-      'Mergulho em barras paralelas'
-    ]
-  },
-  {
-    nome: 'Biceps',
-    id: 'biceps',
-    exercicios: ['Puaxada supinada no pulley', 'Rosca alternada com halteres', 'Rosca testa polia alta', 'Rosca no banco inclinado', 'Rosca Scott'],
-  },
-  {
-    nome: 'Ombro',
-    id: 'ombro',
-    exercicios: ['Desenvolvimento de ombros com halteres', 'Desenvolvimento de ombros com barra', 'Desenvolvimento Arnold para ombros', 'Elevação lateral de ombros com halteres', 'Crucifixo invertido com halteres'],
-  },
-  {
-    nome: 'Treino inferior',
-    id: 'treino-inferior',
-    exercicios: ['Leg Press 45', 'Agachamento', 'Agachamento no Smith', 'Mesa flexora', 'Agachamento sumô'],
-  },
+  nome: 'Peito',
+  id: 'peito',
+  exercicios: [`Supino inclinado com halteres`,
+    `Supino reto com barra`,
+    'Afundos nas barras paralelas',
+    'Crossover com pega alta',
+    'Flexões'
+  ]
+},
+{
+  nome: 'Costas',
+  id: 'costas',
+  exercicios: ['Puxada aberta no pulley', 'Flexão de ombor no cross', 'Remada com corda na polia alta', 'Remada unilateral na polia alta', 'Remada unilateral apoiada no banco'],
+},
+{
+  nome: 'Tríceps',
+  id: 'tricips',
+  exercicios: ['Tríceps na testa',
+    'Tríceps corda na polia',
+    'Apoio no solo com as mãos fechadas(flexão militar)',
+    'Supino fechado',
+    'Mergulho em barras paralelas'
+  ]
+},
+{
+  nome: 'Biceps',
+  id: 'biceps',
+  exercicios: ['Puaxada supinada no pulley', 'Rosca alternada com halteres', 'Rosca testa polia alta', 'Rosca no banco inclinado', 'Rosca Scott'],
+},
+{
+  nome: 'Ombro',
+  id: 'ombro',
+  exercicios: ['Desenvolvimento de ombros com halteres', 'Desenvolvimento de ombros com barra', 'Desenvolvimento Arnold para ombros', 'Elevação lateral de ombros com halteres', 'Crucifixo invertido com halteres'],
+},
+{
+  nome: 'Treino inferior',
+  id: 'treino-inferior',
+  exercicios: ['Leg Press 45', 'Agachamento', 'Agachamento no Smith', 'Mesa flexora', 'Agachamento sumô'],
+},
 ]
 
 function exibirNome() {
